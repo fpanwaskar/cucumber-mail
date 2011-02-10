2c6612f2087fa6510f9a5f1b3011ebbd7c08aff1
Matt Wynne <matt@mattwynne.net>
2011-02-10 14:34:00 +0000

    Switch to chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,6 +8,11 @@ require 'test/unit/assertions'
 
 Capybara.default_driver = :selenium
 Capybara.app_host = "http://10.63.36.213:8080"
+Capybara.default_wait_time = 10
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Driver::Selenium.new(app, :browser => :chrome)
+end
 
 World(Capybara)
 World(Test::Unit::Assertions)
